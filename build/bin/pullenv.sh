469e42e1b6e914082969e75b62d3fdbea29ff032
MickMake <github@mickmake.com>
2020-03-16 18:09:35 +1100

    Rebuild using docker-template 1.0

--- build/bin/pullenv.sh
+++ build/bin/pullenv.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Important for passing the Docker environment through to SSH.
-/etc/gearbox/bin/JsonToConfig -json-string '{}' -template /etc/gearbox/etc/pullenv.tmpl > /etc/environment
+# /etc/gearbox/bin/JsonToConfig -json-string '{}' -template /etc/gearbox/etc/pullenv.tmpl > /etc/environment
+/etc/gearbox/bin/JsonToConfig -template-string '{{ PrintEnv }}' -out /etc/environment
 cp /etc/environment /etc/profile.d/00-gearbox.sh
