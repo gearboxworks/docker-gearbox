a93037b9c5152f1ee884a712086cbff46013c468
MickMake <github@mickmake.com>
2020-03-17 18:58:44 +1100

    removed

--- F/ubuntu-bionic/build/unit-tests/gearbox-base/02-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Created on 2020-03-16T14:41:04+1100, using template:02-release.sh.tmpl and json:gearbox.json
-
-p_info "gearbox-base-ubuntu-bionic" "Release test started."
-
-ps -eaf
-
-p_info "gearbox-base-ubuntu-bionic" "Release test finished."
--- F/ubuntu-xenial/build/unit-tests/gearbox-base/02-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Created on 2020-03-16T14:41:05+1100, using template:02-release.sh.tmpl and json:gearbox.json
-
-p_info "gearbox-base-ubuntu-xenial" "Release test started."
-
-ps -eaf
-
-p_info "gearbox-base-ubuntu-xenial" "Release test finished."
