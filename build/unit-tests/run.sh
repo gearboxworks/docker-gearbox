e152975ee6fe831d34d5df9c5569bbd030e006ee
MickMake <github@mickmake.com>
2020-04-03 17:07:26 +1100

    Update run.sh

--- build/unit-tests/run.sh
+++ build/unit-tests/run.sh
@@ -2,6 +2,7 @@
 
 FILES="$(find /etc/gearbox/unit-tests -mindepth 2 ! -type d | sort)"
 
+EXIT="0"
 for CMD in ${FILES}
 do
 	if [ "${CMD}" == "/etc/gearbox/unit-tests/run.sh" ]
@@ -10,5 +11,13 @@ do
 	fi
 
 	bash -l ${CMD}
+
+	RETURN="$?"
+	echo "RETURN:${RETURN}"
+	if [ "$RETURN" != "0" ]
+	then
+		EXIT="$RETURN"
+	fi
 done
 
+exit ${EXIT}
