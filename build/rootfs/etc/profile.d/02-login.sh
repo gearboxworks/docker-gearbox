80d11a206b767624fa3a59b03c2d0f6d671fd4d8
MickMake <github@mickmake.com>
2021-05-05 02:43:34 +1000

    Update 02-login.sh

--- build/rootfs/etc/profile.d/02-login.sh
+++ build/rootfs/etc/profile.d/02-login.sh
@@ -33,18 +33,18 @@ export PS1
 HISTFILE="${HOME}/.bash_history"
 export HISTFILE
 
-if [ "${SHLVL}" == "1" ]
-then
-	c_cyan "# You have entered the ${GEARBOX_CONTAINER_NAME} Docker container."
-
-	if [ -f "${GEARBOX_PROJECT_DIR}/.NOTMOUNTED" ]
-	then
-		c_warn "# WARNING: Currently the project base directory has NOT been mounted."
-		c_warn "#  - ${GEARBOX_PROJECT_DIR}"
-		echo ""
-	else
-		c_cyan "# All changes made outside of /home/gearbox/projects may not be permanent."
-		echo ""
-	fi
-fi
+#if [ "${SHLVL}" == "1" ]
+#then
+#	c_cyan "# You have entered the ${GEARBOX_CONTAINER_NAME} Docker container."
+#
+#	if [ -f "${GEARBOX_PROJECT_DIR}/.NOTMOUNTED" ]
+#	then
+#		c_warn "# WARNING: Currently the project base directory has NOT been mounted."
+#		c_warn "#  - ${GEARBOX_PROJECT_DIR}"
+#		echo ""
+#	else
+#		c_cyan "# All changes made outside of /home/gearbox/projects may not be permanent."
+#		echo ""
+#	fi
+#fi
 
